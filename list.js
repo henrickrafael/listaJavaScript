bdc06ada97b9372b55fbc85ca55c98797ec5bb19
Rafael H <nevesrafel@hotmail.com>
2022-02-14 23:06:14 -0300

    #004 feat: Reestruturacao Parcial Do Arquivo JS

--- list.js
+++ list.js
@@ -1,41 +1,22 @@
     
     lista = [];
 
-   function createItem(texto) {
+   function createItem(titulo, descricao, data) {
        return {
-        texto: texto
+        titulo: titulo,
+        descricao: descricao,
+        data: data
        }
    } 
 
 
     function addTarefa() {
 
-        var inputItem = document.getElementById("listItem").value;
-        lista.push(createItem(inputItem));
+        var taskName = document.getElementById("taskName").value;
+        var taskDesc = document.getElementById("taskDesc").value;
+        var taskDate = document.getElementById("taskDate").value;
+        lista.push(createItem(taskName, taskDesc, taskDate));
         console.log(lista)
-       
-        let rowId = document.getElementById("rowList");
-        let divBody = document.createElement('div');
-        let divRow = divBody.appendChild(document.createElement('div'));
-        let divTexto = divRow.appendChild(document.createElement('div'));
-        let textoConteudo = divTexto.appendChild(document.createElement('h3'));
-        let divBtnSuccess = divRow.appendChild(document.createElement('div'));
-        let btnSuccess = divBtnSuccess.appendChild(document.createElement('button'));
-        let divBtnDanger = divRow.appendChild(document.createElement('div'));
-        let btnDanger = divBtnDanger.appendChild(document.createElement('button'));
-
-        divBody.classList.add('container', 'mt-2', 'bg-list', 'p-2', 'shadow-sm', 'rounded', 'border');
-        divRow.classList.add('row');
-        divTexto.classList.add('col-md-8', 'text-center');
-        divBtnSuccess.classList.add('col-sm-2');
-        divBtnDanger.classList.add('col-sm-2');
-        btnSuccess.classList.add('mb-1', 'form-control', 'btn', 'btn-success');
-        btnDanger.classList.add('mb-1', 'form-control', 'btn', 'btn-danger');
-
-        btnDanger.textContent = 'Remove';
-        btnSuccess.textContent = 'Done';
-        textoConteudo.textContent = inputItem;
-        rowId.appendChild(divBody);
 
         
     }
