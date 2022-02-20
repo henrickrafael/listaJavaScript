63267189999d9cb60e9738968d650a22680c79a2
Rafael H <nevesrafel@hotmail.com>
2022-02-20 19:42:16 -0300

    #006 feat: Remocao de array e inclusao de funcao para adicionar items a lista dinamicamente

--- list.js
+++ list.js
@@ -1,5 +1,3 @@
-    
-    lista = [];
 
    function createItem(titulo, descricao, data) {
        return {
@@ -15,8 +13,29 @@
         var taskName = document.getElementById("taskName").value;
         var taskDesc = document.getElementById("taskDesc").value;
         var taskDate = document.getElementById("taskDate").value;
-        lista.push(createItem(taskName, taskDesc, taskDate));
-        console.log(lista)
+
+        createItem(taskName, taskDesc, taskDate);
+        
+
+        let item = document.createElement('div');
+        let taskList = document.getElementById('listItem');
+        item.classList.add('container-fluid', 'bg-list', 'mt-2', 'p-2', 'mb-1');
+
+        let header = document.createElement('h3');
+        let taskText = document.createElement('textarea');
+        let deadline = document.createElement('span');
+        taskText.classList.add('readonly', 'form-control', 'mb-2');
+
+        header.innerText = taskName;
+        taskText.innerText = taskDesc;
+        deadline.innerText = `Expira em: ${taskDate}`;
+
+        item.appendChild(header);
+        item.appendChild(taskText);
+        item.appendChild(deadline);
+        taskList.appendChild(item);
 
         
     }
+
+  
